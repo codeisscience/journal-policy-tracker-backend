cf50c6380f3916a2e80c04c6813620e52799f7e0
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2020-10-17 17:30:27 -0300

    Add journal info routes.

--- codecompl.py
+++ codecompl.py
@@ -4,6 +4,7 @@ import sqlite3
 
 from flask import Flask
 
+
 app = Flask(__name__)
 
 
@@ -67,12 +68,13 @@ def initialize_db(path: Path):
     '''))
 
 
-@app.route('/journals', methods=['GET'])
-def list_journals():
-    return ('Nothing here yet. Wanna implement this listing?'
-            ' Submit a PR to <a href='
-            '"https://github.com/codeisscience/codecompliance-backend"'
-            '>the Github repo</a>!')
+STUB_PAGE_MESSAGE = dedent('''
+    Nothing here yet.<br>
+
+    Wanna implement this API function? Submit a PR to <a
+    href="https://github.com/codeisscience/codecompliance-backend">the Github
+    repo</a>!
+    ''')
 
 
 @app.route('/', methods=['GET'])
@@ -87,9 +89,29 @@ def root():
     ''')
 
 
+@app.route('/journals', methods=['GET'])
+def list_journals():
+    '''Lists journals. May receive filters in query parameters.'''
+    # TODO: Use `flask.requests.args` to fetch parameters for:
+    #       - ?keywords=a,b      (comma-separated keyword list)
+    #       - ?keywordcat=code   (specific keyword category)
+    return STUB_PAGE_MESSAGE
+
+
+@app.route('/journals/<identifier>', methods=['GET'])
+def list_journal(identifier):
+    '''Lists general information from a journal, including its domains'''
+    return STUB_PAGE_MESSAGE
+
+
+@app.route('/journals/<identifier>/policies', methods=['GET'])
+def list_journal_policies():
+    '''Lists the policies from a journal.'''
+    return STUB_PAGE_MESSAGE
+
+
 def main():
     initialize_db(Path('journals.db'))
 
 
-print('hooray')
 main()

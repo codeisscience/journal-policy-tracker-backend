dff6aab3836211d4db83fdee759fb2acf02b8259
CIPHERTron <pritish.samal918@gmail.com>
2021-06-17 20:16:15 +0530

    chore: Initialize SQLAlchemy

--- server/__init__.py
+++ server/__init__.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///journals.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+def create_db():
+    db = SQLAlchemy(app)
+    return db
+
 
 from .routes import journal

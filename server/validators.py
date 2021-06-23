b458180d70bb98ca6462742f906f0a75153be526
CIPHERTron <pritish.samal918@gmail.com>
2021-06-24 00:47:09 +0530

    chore: Add Login Form Validators

--- server/validators.py
+++ server/validators.py
@@ -16,3 +16,10 @@ class RegistrationForm(FlaskForm):
         "Confirm Password", validators=[DataRequired(), EqualTo("Password")]
     )
     submit = SubmitField("Sign Up")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Login")

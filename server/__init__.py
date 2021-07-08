f0f5250699551653123a4587ac88c8896c5a4f16
CIPHERTron <pritish.samal918@gmail.com>
2021-07-08 19:31:55 +0530

    chore: Add SECRET_KEY to App Config

--- server/__init__.py
+++ server/__init__.py
@@ -5,6 +5,7 @@ from flask_login import LoginManager, login_manager
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "68a2c99120b7e9558c126213b6989f1d"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///journals.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 

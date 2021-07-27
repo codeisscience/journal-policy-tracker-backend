e3ef1a4c1e78425f341e2e9f1474556fc3077d20
CIPHERTron <pritish.samal918@gmail.com>
2021-07-27 23:55:57 +0530

    chore: Add SECRET_KEY in .env file

--- server/__init__.py
+++ server/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
+import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_manager
 
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = "68a2c99120b7e9558c126213b6989f1d"
+app.config["SECRET_KEY"] = SECRET_KEY
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///journals.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 

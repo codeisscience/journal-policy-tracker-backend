84d8f2e62c90a00d93e7520106ecc80b7f89dc82
CIPHERTron <pritish.samal918@gmail.com>
2021-07-30 11:01:10 +0530

    fix: Display Message To Generate SECRET_KEY if it is not set

--- server/__init__.py
+++ server/__init__.py
@@ -1,11 +1,24 @@
 import os
+import sys
 
+from textwrap import dedent
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_manager
 
 SECRET_KEY = os.getenv("SECRET_KEY")
+
+if SECRET_KEY is None:
+    sys.exit(
+        dedent(
+            """
+        SECRET_KEY is not set. Hint: create a .env file containing:
+        SECRET_KEY=<randomly generated key>
+        To generate a key, see https://flask.palletsprojects.com/en/latest/config/#SECRET_KEY.
+    """
+        ).strip()
+    )
 
 app = Flask(__name__)
 

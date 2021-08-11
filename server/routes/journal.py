84f1f4d5cc437fbe68f4e85972563ba78950c06b
CIPHERTron <pritish.samal918@gmail.com>
2021-08-11 22:09:37 +0530

    Enable CORS For All Endpoints & Minor Fixes

--- server/routes/journal.py
+++ server/routes/journal.py
@@ -1,5 +1,6 @@
 from server import app
 from textwrap import dedent
+from flask_cors import cross_origin
 
 
 STUB_PAGE_MESSAGE = dedent(
@@ -14,6 +15,7 @@ STUB_PAGE_MESSAGE = dedent(
 
 
 @app.route("/", methods=["GET"])
+@cross_origin()
 def root():
     return dedent(
         """
@@ -30,6 +32,7 @@ def root():
 
 
 @app.route("/api/journals", methods=["GET"])
+@cross_origin()
 def list_journals():
     """Lists journals. May receive filters in query parameters."""
     # TODO: Use `flask.requests.args` to fetch parameters for:
@@ -39,12 +42,14 @@ def list_journals():
 
 
 @app.route("/api/journals/<identifier>", methods=["GET"])
+@cross_origin()
 def list_journal(identifier):
     """Lists general information from a journal, including its domains"""
     return STUB_PAGE_MESSAGE
 
 
 @app.route("/api/journals/<identifier>/policies", methods=["GET"])
+@cross_origin()
 def list_journal_policies():
     """Lists the policies from a journal."""
     return STUB_PAGE_MESSAGE

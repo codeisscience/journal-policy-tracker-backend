5f8b6abc66a8ca6ac609f81bfee4f9dcbfbe171a
CIPHERTron <pritish.samal918@gmail.com>
2021-08-12 20:01:37 +0530

    fix: Update Journal Models

--- server/models/Journal.py
+++ server/models/Journal.py
@@ -1,14 +1,17 @@
 from datetime import datetime
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import backref
 
 from server import db
 
 
 class Journal(db.Model):
+    __tablename__ = "journal"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60), unique=True, nullable=False)
     url = db.Column(db.Text(2000), unique=True, nullable=False)
     issn = db.Column(db.Integer, unique=True, nullable=False)
-    about = db.relationship("Rating", backref="author", lazy=True)
+    ratings = db.Column(db.Integer, ForeignKey("ratings.rating"))
     # policies = db.relationship("Policies", backref="author", lazy=True)
     # domains = db.relationship("Domain", backref="author", lazy=True)
 
@@ -17,9 +20,11 @@ class Journal(db.Model):
 
 
 class Rating(db.Model):
+    __tablename__ = "ratings"
     id = db.Column(db.Integer, primary_key=True)
     rating = db.Column(db.Integer, nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    ratings = db.relationship("Journal", backref="rating")
 
     def __repr__(self):
         return f"Rating('{self.rating}', '{self.date}')"
@@ -27,9 +32,11 @@ class Rating(db.Model):
 
 class Policies(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    issn = db.Column(db.Integer, nullable=False)
     title = db.Column(db.String(50), nullable=False)
     first_year = db.Column(db.Integer, nullable=False)
     last_year = db.Column(db.Integer, nullable=True)
+    policy_type = db.Column(db.String(30), nullable=False)
 
     def __repr__(self):
         return f"Policies('{self.title}', '{self.first_year}', '{self.last_year}')"
@@ -37,6 +44,7 @@ class Policies(db.Model):
 
 class Domain(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    issn = db.Column(db.Integer, nullable=False)
     name = db.Column(db.String(20), nullable=False)
 
     def __repr__(self):

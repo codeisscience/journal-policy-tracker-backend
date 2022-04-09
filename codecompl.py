af678f9e7aa2fc7bec3111516e02838ec0590157
erik-whiting <erik@erikwhiting.com>
2022-04-09 00:37:36 -0500

    Build database when app starts

--- codecompl.py
+++ codecompl.py
@@ -1,4 +1,8 @@
-from server import app
+from server import app, db
+
+# Import model definitions and then create the database
+db.create_all()
+db.session.commit()
 
 if __name__ == "__main__":
     app.run(debug=True)

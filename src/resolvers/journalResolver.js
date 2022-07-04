a378e920b196add4d9b384006bbe204e8b34a6dc
Devesh <deves125@gmail.com>
2022-07-04 22:28:38 +0530

    feat(journal-crud): basic journal crud - #54 This commit contains the following:  - Updated Journal model and typeDefs  - getJournalByISSN Query  - deleteJournal Mutation  - updateJournal Mutation

--- src/resolvers/journalResolver.js
+++ src/resolvers/journalResolver.js
@@ -5,13 +5,33 @@ const journalResolver = {
     getAllJournals: async () => {
       return await Journal.find();
     },
+
+    getJournalByISSN: async (_, { issn }) => {
+      return await Journal.findOne({ issn });
+    },
   },
+
   Mutation: {
     createJournal: async (_, args) => {
-      const { title, url, issn, domain } = args.journal;
-      const journal = new Journal({ title, url, issn, domain });
+      const { title, url, issn, domainName, policies } = args.journal;
+      const journal = new Journal({ title, url, issn, domainName, policies });
       await journal.save();
       return journal;
+    },
+
+    deleteJournal: async (_, { issnToDelete }) => {
+      await Journal.deleteOne({ issn: issnToDelete });
+      return "Journal Deleted";
+    },
+
+    updateJournal: async (_, { issnToUpdate, newJournalDetails }) => {
+      const { title, url, domainName, issn, policies } = newJournalDetails;
+      const { _id } = await Journal.findOne({ issn: issnToUpdate });
+      await Journal.updateOne(
+        { _id },
+        { title, url, domainName, issn, policies }
+      );
+      return await Journal.findOne({ _id });
     },
   },
 };

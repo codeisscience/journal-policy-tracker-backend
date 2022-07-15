c70f7e9458fa87d3bc73076756e188190a067bde
Devesh <deves125@gmail.com>
2022-07-16 00:01:33 +0530

    feat(journal-crud): updateJournal error handling updateJournal will return an appropriate error when we try to update a journal that is not present in the database.

--- src/resolvers/journalResolver.js
+++ src/resolvers/journalResolver.js
@@ -38,20 +38,26 @@ const journalResolver = {
     },
 
     updateJournal: async (_, { issnToUpdate, newJournalDetails }) => {
-      const { _id } = await Journal.findOne({ issn: issnToUpdate });
+      const journalToUpdate = await Journal.findOne({ issn: issnToUpdate });
 
+      if (!journalToUpdate) {
+        return {
+          errors: [{ field: "issn", message: "issn not available" }],
+        };
+      }
+
+      const { _id } = journalToUpdate;
       try {
         await Journal.updateOne({ _id }, { ...newJournalDetails });
       } catch (error) {
         if (error.code === 11000 && Object.keys(error.keyValue)[0] === "issn") {
           return {
-            errors: [{ field: "issn", message: "issn already exists" }],
+            errors: [{ field: "issn", message: "new issn already exists" }],
           };
         }
       }
 
       const updatedJournal = await Journal.findOne({ _id });
-
       return { journal: updatedJournal };
     },
   },

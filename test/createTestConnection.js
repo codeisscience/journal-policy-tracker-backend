6565ff26305cce0c7147606cbcb5142ba99309ee
Devesh <deves125@gmail.com>
2022-09-06 01:57:17 +0530

    fix(test): change to mongoose.connect()

--- test/createTestConnection.js
+++ test/createTestConnection.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 
-export const createTestConnection = async () =>
-  mongoose.createConnection("mongodb://localhost:27017/testingDatabase");
+// export const createTestConnection = async () =>
+//   mongoose.createConnection("mongodb://localhost:27017/testingDatabase");
 
-// export const createTestConnection = mongoose.connect(
-//   "mongodb://localhost:27017/testingDatabase"
-// );
+export const createTestConnection = async () =>
+  mongoose.connect("mongodb://localhost:27017/testingDatabase");

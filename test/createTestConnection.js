fac01b06259f52c2ee9e0f27a77c7e13c9957753
Devesh <deves125@gmail.com>
2022-09-06 16:22:49 +0530

    chore(test): remove commented code

--- test/createTestConnection.js
+++ test/createTestConnection.js
@@ -1,7 +1,4 @@
 import mongoose from "mongoose";
 
-// export const createTestConnection = async () =>
-//   mongoose.createConnection("mongodb://localhost:27017/testingDatabase");
-
 export const createTestConnection = async () =>
   mongoose.connect("mongodb://localhost:27017/testingDatabase");

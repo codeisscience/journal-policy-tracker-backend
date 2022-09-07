4861342833211664d93d3de3df5b5d6b170ac72a
Devesh <deves125@gmail.com>
2022-09-07 12:42:57 +0530

    test: add testing data for journals

---
+++ test/journalsData.js
@@ -0,0 +1,171 @@
+export const testJournal1 = {
+  journal: {
+    title: "Journal Title",
+    url: "https://journalurl1234.com",
+    issn: "11111111",
+    domainName: "Domain Name",
+    policies: {
+      title: "Policy Title",
+      firstYear: 2010,
+      lastYear: 9999,
+      policyType: "NUMBER_TWO",
+      isDataAvailabilityStatementPublished: false,
+      isDataShared: false,
+      isDataPeerReviewed: false,
+      enforced: "SOMETIMES",
+      enforcedEvidence: "Enforced Evidence String",
+    },
+  },
+};
+
+export const testJournal2 = {
+  journal: {
+    title: "Journal Title 2",
+    url: "https://journalurl1234.com",
+    issn: "22222222",
+    domainName: "Domain Name",
+    policies: {
+      title: "Policy Title",
+      firstYear: 2010,
+      lastYear: 9999,
+      policyType: "NUMBER_TWO",
+      isDataAvailabilityStatementPublished: false,
+      isDataShared: false,
+      isDataPeerReviewed: false,
+      enforced: "SOMETIMES",
+      enforcedEvidence: "Enforced Evidence String",
+    },
+  },
+};
+
+export const testJournal3 = {
+  journal: {
+    title: "Journal Title 3",
+    url: "https://journalurl1234.com",
+    issn: "33333333",
+    domainName: "Domain Name",
+    policies: {
+      title: "Policy Title",
+      firstYear: 2010,
+      lastYear: 9999,
+      policyType: "NUMBER_TWO",
+      isDataAvailabilityStatementPublished: false,
+      isDataShared: false,
+      isDataPeerReviewed: false,
+      enforced: "SOMETIMES",
+      enforcedEvidence: "Enforced Evidence String",
+    },
+  },
+};
+
+export const testJournal4 = {
+  journal: {
+    title: "Journal Title 4",
+    url: "https://journalurl1234.com",
+    issn: "44444444",
+    domainName: "Domain Name",
+    policies: {
+      title: "Policy Title",
+      firstYear: 2010,
+      lastYear: 9999,
+      policyType: "NUMBER_TWO",
+      isDataAvailabilityStatementPublished: false,
+      isDataShared: false,
+      isDataPeerReviewed: false,
+      enforced: "SOMETIMES",
+      enforcedEvidence: "Enforced Evidence String",
+    },
+  },
+};
+
+export const updatedJournal = {
+  title: "Updated Journal Title 1",
+  url: "https://journalurl1234.com",
+  issn: "12121212",
+  domainName: "Domain Name",
+  policies: {
+    title: "Policy Title",
+    firstYear: 2010,
+    lastYear: 9999,
+    policyType: "NUMBER_TWO",
+    isDataAvailabilityStatementPublished: false,
+    isDataShared: false,
+    isDataPeerReviewed: false,
+    enforced: "SOMETIMES",
+    enforcedEvidence: "Enforced Evidence String",
+  },
+};
+
+export const journalsArray = [
+  {
+    title: "Journal Title",
+    url: "https://journalurl1234.com",
+    issn: "11111111",
+    domainName: "Domain Name",
+    policies: {
+      title: "Policy Title",
+      firstYear: 2010,
+      lastYear: 9999,
+      policyType: "NUMBER_TWO",
+      isDataAvailabilityStatementPublished: false,
+      isDataShared: false,
+      isDataPeerReviewed: false,
+      enforced: "SOMETIMES",
+      enforcedEvidence: "Enforced Evidence String",
+    },
+  },
+
+  {
+    title: "Journal Title 2",
+    url: "https://journalurl1234.com",
+    issn: "22222222",
+    domainName: "Domain Name",
+    policies: {
+      title: "Policy Title",
+      firstYear: 2010,
+      lastYear: 9999,
+      policyType: "NUMBER_TWO",
+      isDataAvailabilityStatementPublished: false,
+      isDataShared: false,
+      isDataPeerReviewed: false,
+      enforced: "SOMETIMES",
+      enforcedEvidence: "Enforced Evidence String",
+    },
+  },
+
+  {
+    title: "Journal Title 3",
+    url: "https://journalurl1234.com",
+    issn: "33333333",
+    domainName: "Domain Name",
+    policies: {
+      title: "Policy Title",
+      firstYear: 2010,
+      lastYear: 9999,
+      policyType: "NUMBER_TWO",
+      isDataAvailabilityStatementPublished: false,
+      isDataShared: false,
+      isDataPeerReviewed: false,
+      enforced: "SOMETIMES",
+      enforcedEvidence: "Enforced Evidence String",
+    },
+  },
+
+  {
+    title: "Journal Title 4",
+    url: "https://journalurl1234.com",
+    issn: "44444444",
+    domainName: "Domain Name",
+    policies: {
+      title: "Policy Title",
+      firstYear: 2010,
+      lastYear: 9999,
+      policyType: "NUMBER_TWO",
+      isDataAvailabilityStatementPublished: false,
+      isDataShared: false,
+      isDataPeerReviewed: false,
+      enforced: "SOMETIMES",
+      enforcedEvidence: "Enforced Evidence String",
+    },
+  },
+];

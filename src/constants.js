08565d741a867e81e8f1f8a51407756c0de21f86
Devesh <deves125@gmail.com>
2022-12-06 21:14:34 +0530

    feat(user-auth): add links for email to constants

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,8 @@
 export const COOKIE_NAME = "obfc";
 export const __prod__ = process.env.NODE_ENV === "production";
+export const FORGET_PASSWORD_PREFIX = "forget-password:";
+export const LOGO_URL =
+  "https://codeisscience-community.netlify.app/img/brand.png";
+export const CENTER_IMAGE_URL =
+  "https://user-images.githubusercontent.com/59534570/205953467-e60d4683-ea80-40c0-9aa2-b885663de0d3.png";
+export const CODE_IS_SCIENCE_URL = "https://codeisscience.com";

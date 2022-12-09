3b6d5370a3e7463964894b7287318b22287e17e0
Devesh <deves125@gmail.com>
2022-12-09 18:41:00 +0530

    feat(user-auth): add email constants Separated email constants from app constants and added more email constants to pass in content in the new email template.

---
+++ src/utils/emailConstants.js
@@ -0,0 +1,36 @@
+// ? Common Email Constants
+export const LOGO_URL =
+  "https://codeisscience-community.netlify.app/img/brand.png";
+export const CODE_IS_SCIENCE_URL = "https://codeisscience.com";
+
+// ? Forgot Password Email Constants
+export const FORGOT_PASSWORD_EMAIL_TITLE = "Reset Your Password";
+
+export const FORGOT_PASSWORD_IMAGE_URL =
+  "https://user-images.githubusercontent.com/59534570/205953467-e60d4683-ea80-40c0-9aa2-b885663de0d3.png";
+export const FORGOT_PASSWORD_IMAGE_URL_ALT = "Forgot your password?";
+
+export const FORGOT_PASSWORD_TITLE_ONE = "FORGOT";
+export const FORGOT_PASSWORD_TITLE_TWO = "YOUR PASSWORD?";
+export const FORGOT_PASSWORD_SUBTITLE =
+  "Not to worry, we got you! Let’s get you a new password.";
+
+export const FORGOT_PASSWORD_BUTTON_TEXT = "RESET PASSWORD";
+export const FORGOT_PASSWORD_BUTTON_POINTER_TEXT =
+  "Reset your Code Is Science password";
+
+// ? Account Verification Email Constants
+export const ACCOUNT_VERIFICATION_EMAIL_TITLE = "Verify Your Account";
+
+export const ACCOUNT_VERIFICATION_IMAGE_URL =
+  "https://user-images.githubusercontent.com/59534570/205953467-e60d4683-ea80-40c0-9aa2-b885663de0d3.png";
+export const ACCOUNT_VERIFICATION_IMAGE_URL_ALT = "Verify your account";
+
+export const ACCOUNT_VERIFICATION_TITLE_ONE = "WELCOME TO";
+export const ACCOUNT_VERIFICATION_TITLE_TWO = "CODE IS SCIENCE";
+export const ACCOUNT_VERIFICATION_SUBTITLE =
+  "Press the button below to verify your email address.";
+
+export const ACCOUNT_VERIFICATION_BUTTON_TEXT = "VERIFY ACCOUNT";
+export const ACCOUNT_VERIFICATION_BUTTON_POINTER_TEXT =
+  "Verify your Code Is Science account";

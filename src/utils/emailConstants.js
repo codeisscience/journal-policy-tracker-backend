65cad8d7ae3fb51d24c12660255d5c59c4a9f11b
Devesh <deves125@gmail.com>
2022-12-16 23:06:13 +0530

    feat(user-auth): add email verification constants This adds all the email constants that will go in the email which will be sent to the user's new email address for verification.

--- src/utils/emailConstants.js
+++ src/utils/emailConstants.js
@@ -34,3 +34,19 @@ export const ACCOUNT_VERIFICATION_SUBTITLE =
 export const ACCOUNT_VERIFICATION_BUTTON_TEXT = "VERIFY ACCOUNT";
 export const ACCOUNT_VERIFICATION_BUTTON_POINTER_TEXT =
   "Verify your Code Is Science account";
+
+// ? New Email Address Verification Email Constants
+
+export const NEW_EMAIL_ADDRESS_VERIFICATION_EMAIL_TITLE =
+  "Verify Your New Email Address";
+export const NEW_EMAIL_ADDRESS_VERIFICATION_IMAGE_URL =
+  "https://user-images.githubusercontent.com/59534570/205953467-e60d4683-ea80-40c0-9aa2-b885663de0d3.png";
+export const NEW_EMAIL_ADDRESS_VERIFICATION_IMAGE_URL_ALT =
+  "Verify your new email address";
+export const NEW_EMAIL_ADDRESS_VERIFICATION_TITLE_ONE = "VERIFY YOUR";
+export const NEW_EMAIL_ADDRESS_VERIFICATION_TITLE_TWO = "NEW EMAIL ADDRESS";
+export const NEW_EMAIL_ADDRESS_VERIFICATION_SUBTITLE =
+  "Press the button below to verify your new email address.";
+export const NEW_EMAIL_ADDRESS_VERIFICATION_BUTTON_TEXT = "VERIFY EMAIL";
+export const NEW_EMAIL_ADDRESS_VERIFICATION_BUTTON_POINTER_TEXT =
+  "Verify your new email address";

14603076bd01c26d27183e9b2dfa55063afdae6e
Devesh <deves125@gmail.com>
2022-12-16 23:07:07 +0530

    feat(user-auth): add verify new email add prefix

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,4 @@ export const COOKIE_NAME = "obfc";
 export const __prod__ = process.env.NODE_ENV === "production";
 export const FORGET_PASSWORD_PREFIX = "forget-password:";
 export const ACCOUNT_VERIFICATION_PREFIX = "account-verification:";
+export const VERIFY_NEW_EMAIL_ADDRESS_PREFIX = "verify-new-email-address:";
